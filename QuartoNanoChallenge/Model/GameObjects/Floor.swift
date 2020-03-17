ba9f1d0728c11af568f9d9b968e1d9ecf90a643c
Carlos Modinez <modinez.carlos@gmail.com>
2020-03-17 15:16:44 -0300

    Sons de contato com as plataformas, de contato com a água, de swipe e tap

--- QuartoNanoChallenge/Model/GameObjects/Floor.swift
+++ QuartoNanoChallenge/Model/GameObjects/Floor.swift
@@ -10,25 +10,6 @@ import SpriteKit
 
 class Floor: SKSpriteNode, GameObject {
     var floor = SKSpriteNode(color: .red, size: CGSize(width: 600, height: 100))
-
-    func createFirstFloor() {
-        floor.position = CGPoint(x: 0, y: -500)
-        floor.zPosition = 3.0
-        
-        let contactMask = SKPhysicsBody(rectangleOf: floor.size)
-        floor.physicsBody = contactMask
-        floor.physicsBody?.affectedByGravity = false
-        floor.physicsBody?.isDynamic = false
-        floor.physicsBody?.allowsRotation = false
-        floor.physicsBody?.friction = 1.0
-        floor.physicsBody?.linearDamping = 1.0
-       
-        //Quem é ele na mascara
-        floor.physicsBody!.categoryBitMask = BodyMasks.floor
-       
-        //Com quem colide
-        floor.physicsBody!.collisionBitMask = BodyMasks.player
-    }
     
     func sortFloor(position: CGPoint, size: CGSize, withCoins: Bool, type: floorType){
         
@@ -50,19 +31,24 @@ class Floor: SKSpriteNode, GameObject {
         if type == .ice {
             floor.physicsBody?.friction = 0.08
             floor.texture = SKTexture(imageNamed: "ice")
+            floor.physicsBody?.categoryBitMask = BodyMasks.ice
         } else if type == .dirt {
             floor.physicsBody?.friction = 0.5
             floor.texture = SKTexture(imageNamed: "dirt")
+            floor.physicsBody?.categoryBitMask = BodyMasks.dirt
         } else {
             floor.physicsBody?.friction = 1.0
+            floor.physicsBody?.categoryBitMask = BodyMasks.rock
             floor.texture = SKTexture(imageNamed: "rock")
         }
-            
-        //Quem é ele na mascara
-        floor.physicsBody!.categoryBitMask = BodyMasks.floor
+        
+        //Com quem ele tem contato
+        floor.physicsBody?.contactTestBitMask = BodyMasks.player | BodyMasks.box
         
         //Com quem colide
         floor.physicsBody!.collisionBitMask = BodyMasks.player | BodyMasks.box
+        
+        
     }
     
     func update(deltaTime daltaTime: TimeInterval, velocity: Double) {
