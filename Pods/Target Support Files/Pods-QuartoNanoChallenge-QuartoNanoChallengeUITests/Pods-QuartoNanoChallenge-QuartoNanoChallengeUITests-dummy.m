09179062c9205c4929a17367a1bfda2c76224443
Akira Tsukamoto <akiratsukamoto13@gmail.com>
2020-03-17 21:34:35 -0300

    analytics pod installed

---
+++ Pods/Target Support Files/Pods-QuartoNanoChallenge-QuartoNanoChallengeUITests/Pods-QuartoNanoChallenge-QuartoNanoChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuartoNanoChallenge_QuartoNanoChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_QuartoNanoChallenge_QuartoNanoChallengeUITests
+@end
